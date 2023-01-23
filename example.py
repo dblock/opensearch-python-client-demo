5ed7b24a4dce7e4e04ccc4ae68916b718b343333
dblock <dblock@amazon.com>
2023-01-23 09:26:50 -0500

    WIP: async demo.
    Signed-off-by: dblock <dblock@amazon.com>

--- example.py
+++ example.py
@@ -7,12 +7,13 @@
 # Modifications Copyright OpenSearch Contributors. See
 # GitHub history for details.
 
+import asyncio
 from os import environ
 from time import sleep
 from urllib.parse import urlparse
 
 from boto3 import Session
-from opensearchpy import AWSV4SignerAuth, OpenSearch, RequestsHttpConnection
+from opensearchpy import AWSV4SignerAsyncAuth, AsyncOpenSearch, AsyncHttpConnection
 
 # cluster endpoint, for example: my-test-domain.us-east-1.es.amazonaws.com
 url = urlparse(environ['ENDPOINT'])
@@ -20,9 +21,9 @@ region = environ.get('AWS_REGION', 'us-east-1')
 
 credentials = Session().get_credentials()
 
-auth = AWSV4SignerAuth(credentials, region)
+auth = AWSV4SignerAsyncAuth(credentials, region)
 
-client = OpenSearch(
+client = AsyncOpenSearch(
   hosts=[{
     'host': url.netloc,
     'port': url.port or 443
@@ -30,31 +31,39 @@ client = OpenSearch(
   http_auth=auth,
   use_ssl=True,
   verify_certs=True,
-  connection_class=RequestsHttpConnection
+  connection_class=AsyncHttpConnection
 )
 
-info = client.info()
-print(f"{info['version']['distribution']}: {info['version']['number']}")
+async def main():
+  info = await client.info()
+  print(f"{info['version']['distribution']}: {info['version']['number']}")
 
-# create an index
-index = 'movies'
-client.indices.create(index=index)
+  # create an index
+  index = 'movies3'
+  await client.indices.create(index=index)
 
-try:
-  # index data
-  document = {'director': 'Bennett Miller', 'title': 'Moneyball', 'year': 2011}
-  client.index(index=index, body=document, id='1', refresh=True)
+  try:
+    # index data
+    document = {'director': 'Bennett Miller', 'title': 'Moneyball', 'year': 2011}
+    await client.index(index=index, body=document, id='1')
 
-  # wait for the document to index
-  sleep(1)
+    # wait for the document to index
+    sleep(1)
 
-  # search for the document
-  results = client.search(body={'query': {'match': {'director': 'miller'}}})
-  for hit in results['hits']['hits']:
-    print(hit['_source'])
+    # search for the document
+    results = await client.search(body={'query': {'match': {'director': 'miller'}}})
+    for hit in results['hits']['hits']:
+      print(hit['_source'])
 
-  # delete the document
-  client.delete(index=index, id='1')
-finally:
-  # delete the index
-  client.indices.delete(index=index)
+    # delete the document
+    await client.delete(index=index, id='1')
+  finally:
+    # delete the index
+    await client.indices.delete(index=index)
+    await client.close()
+
+
+if __name__ == "__main__":   
+  loop = asyncio.get_event_loop()
+  loop.run_until_complete(main())
+  loop.close()
